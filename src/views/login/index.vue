3dd6ef90848fa1d2a5518ea110b6c57a250b6038
BkonKing <33289637+BkonKing@users.noreply.github.com>
2020-12-25 17:14:28 +0800

    UI：登录UI改版

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,97 +1,95 @@
 <template>
-  <div class="page">
+  <div class="login-page">
+    <div class="logo-header"></div>
     <i
       v-if="status"
       class="van-icon van-icon-arrow-left van-nav-bar__arrow"
       @click="$router.go(-1)"
     ></i>
-    <img class="logo" src="@/assets/imgs/login_logo.png" />
-    <img class="logo-text" src="@/assets/imgs/login_logo_text.png" />
-    <div class="form">
-      <div class="form-body">
-        <Field
-          v-model="mobile"
-          class="form-input"
-          maxlength="11"
-          type="tel"
-          placeholder="请输入您的手机号"
-        ></Field>
-        <Field
-          v-if="login_type === 1"
+    <div class="logo-container">
+      <img class="logo" src="@/assets/imgs/login_logo.png" />
+    </div>
+    <div class="form-container">
+      <van-field
+        v-model="mobile"
+        class="form-input"
+        maxlength="11"
+        type="tel"
+        placeholder="手机号"
+      ></van-field>
+      <div v-if="loginType === 1" class="code-cell">
+        <van-field
           v-model="yzm"
           key="code"
-          class="form-input"
-          maxlength="11"
+          class="form-input tf-flex-item"
+          maxlength="4"
           type="digit"
           placeholder="验证码"
         >
-          <template #button>
+        </van-field>
+        <van-button
+          class="query-code-btn"
+          :class="{ 'code-count-down': codeStatus }"
+          :loading="codeLoading"
+          :disabled="codeStatus"
+          @click="verifCode"
+        >
+          <template #default>
             <van-count-down
               v-if="codeStatus"
               :time="countDownTime"
               @finish="countFinish"
             >
               <template #default="timeData">
-                <span class="tf-text-white">{{ timeData.seconds }}s</span>
+                <span>{{ timeData.seconds }}s</span>
               </template>
             </van-count-down>
-            <van-button v-else class="query-btn" :loading="codeLoading" @click="verifCode"
-              >获取</van-button
-            >
+            <span v-else>获取验证码</span>
           </template>
-        </Field>
-        <Field
-          v-if="login_type === 2"
-          v-model="pwd"
-          class="form-input"
-          key="password"
-          :type="showPassword ? 'text' : 'password'"
-          placeholder="请输入您的密码"
-        >
-          <template #button>
-            <span
-              class="tf-icon tf-text-white"
-              :class="[showPassword ? 'tf-icon-kejian' : 'tf-icon-bukejian']"
-              @click="changePassword"
-            ></span>
-          </template>
-        </Field>
+        </van-button>
       </div>
-    </div>
-    <van-button
-      v-preventReClick
-      :loading="loginLoading"
-      class="login-btn"
-      @click="login"
-      >登 录</van-button
-    >
-    <span
-      v-if="login_type === 1"
-      class="login-method__text"
-      @click="login_type = 2"
-      >密码登录</span
-    >
-    <span
-      v-else-if="login_type === 2"
-      class="login-method__text"
-      @click="login_type = 1"
-      >验证码登录</span
-    >
-    <div class="agreement" @click="changeRememberPasswrod">
-      <div class="uni-checkbox-input">
-        <span
-          class="tf-icon checkbox-icon"
-          :class="{ 'tf-icon-gou': agree }"
-        ></span>
+      <van-field
+        v-if="loginType === 2"
+        v-model="pwd"
+        class="form-input"
+        key="password"
+        :type="showPassword ? 'text' : 'password'"
+        placeholder="密码"
+      >
+        <template #button>
+          <span
+            class="tf-icon"
+            :class="[showPassword ? 'tf-icon-kejian' : 'tf-icon-bukejian']"
+            @click="showPassword = !showPassword"
+          ></span>
+        </template>
+      </van-field>
+      <van-button
+        v-preventReClick
+        :loading="loginLoading"
+        class="login-btn"
+        @click="submitLogin"
+        >登 录</van-button
+      >
+      <span
+        class="login-method__text"
+        @click="loginType = loginType === 1 ? 2 : 1"
+        >{{ loginType === 1 ? "密码登录" : "验证码登录" }}</span
+      >
+      <div class="agreement" @click="agree = !agree">
+        <div class="agreement-checkbox-input">
+          <span
+            class="tf-icon checkbox-icon"
+            :class="{ 'tf-icon-gou': agree }"
+          ></span>
+        </div>
+        <span class="agreement-text">
+          登录即表示您同意
+          <router-link class="agreement-link" to="/agreement"
+            >《美好生活家园用户协议》</router-link
+          >
+        </span>
       </div>
-      <span class="agreement-text" style="color: #fff;">
-        登录即表示您同意
-        <router-link
-          style="color: #fff;text-decoration: underline;"
-          to="/agreement"
-          >《美好生活家园用户协议》</router-link
-        >
-      </span>
     </div>
   </div>
 </template>
@@ -104,7 +102,7 @@ import { hasPermission, reqPermission } from '@/utils/permission'
 import { setStatisticsData } from '@/utils/analysis.js'
 export default {
   components: {
-    Field,
+    [Field.name]: Field,
     [Button.name]: Button,
     [CountDown.name]: CountDown,
     [NavBar.name]: NavBar
@@ -112,16 +110,16 @@ export default {
   data () {
     return {
       mobile: undefined,
-      yzm: undefined,
-      pwd: undefined,
-      login_type: 1, // 1:验证码登录 2：密码登陆
+      yzm: undefined, // 验证码
+      pwd: undefined, // 密码
+      loginType: 1, // 1:验证码登录 2：密码登陆
       agree: true, // 同意协议
       showPassword: false,
-      codeLoading: false,
-      codeStatus: false,
+      codeLoading: false, // 获取验证loading状态
+      codeStatus: false, // 验证码发送状态
       countDownTime: 60000,
-      status: 0,
-      loginLoading: false
+      status: 0, // 1：换个账号登录 0：登录
+      loginLoading: false // 登录loading状态
     }
   },
   created () {
@@ -145,25 +143,15 @@ export default {
     }
   },
   methods: {
-    // 定位未开启提醒
-    warnLoacation () {
-      Toast('没有开启定位，可能会影响部分功能哦！')
-    },
-    changeRememberPasswrod () {
-      this.agree = !this.agree
-    },
-    changePassword () {
-      this.showPassword = !this.showPassword
-    },
-    /* 登录 */
-    login () {
+    // 提交登录验证
+    submitLogin () {
       if (!this.agree) {
         Toast('请阅读并同意用户协议')
         return
       }
-      let params = {}
+      let params
       /* 验证码登录 */
-      if (this.login_type === 1) {
+      if (this.loginType === 1) {
         if (
           validEmpty(this.mobile, '请输入手机号码') ||
           validEmpty(this.yzm, '请输入验证码')
@@ -174,7 +162,7 @@ export default {
           mobile: this.mobile,
           yzm: this.yzm
         }
-      } else if (this.login_type === 2) {
+      } else if (this.loginType === 2) {
         /* 密码登录 */
         if (
           validEmpty(this.mobile, '请输入手机号码') ||
@@ -187,39 +175,20 @@ export default {
           pwd: this.pwd
         }
       }
+      this.login(params)
+    },
+    // 登录请求
+    login (params) {
       this.loginLoading = true
       this.$store
         .dispatch('login', {
-          type: this.login_type,
+          type: this.loginType,
           params
         })
         .then(data => {
           this.loginLoading = false
-          const share_params = this.$store.state.share_params || ''
-          if (share_params) {
-            this.$store.commit('setShare_params', '')
-            if (share_params.page_type == 1) {
-              this.$router.replace({
-                path: '/store/goods-detail',
-                query: {
-                  id: share_params.id,
-                  f_id: share_params.f_id
-                }
-              })
-            } else if (share_params.page_type == 2) {
-              this.$router.replace({
-                path: '/pages/neighbours/details',
-                query: {
-                  articleType: share_params.articleType,
-                  id: share_params.id
-                }
-              })
-            }
-          } else {
-            this.$router.replace({
-              name: 'home'
-            })
-          }
+          this.loginJump()
+          // 行为数据分析收集
           if (data.first_register == 1) {
             this.mtjEvent({
               eventId: 2
@@ -235,23 +204,53 @@ export default {
           this.loginLoading = false
         })
     },
-    /* 发送验证码 */
+    // 登录跳转，判断是否通过分享进入未登录情况下吗，登录成功后跳转到分享页面，否则跳转到首页
+    loginJump () {
+      const share_params = this.$store.state.share_params || ''
+      if (share_params) {
+        this.$store.commit('setShare_params', '')
+        if (share_params.page_type == 1) {
+          this.$router.replace({
+            path: '/store/goods-detail',
+            query: {
+              id: share_params.id,
+              f_id: share_params.f_id
+            }
+          })
+        } else if (share_params.page_type == 2) {
+          this.$router.replace({
+            path: '/pages/neighbours/details',
+            query: {
+              articleType: share_params.articleType,
+              id: share_params.id
+            }
+          })
+        }
+      } else {
+        this.$router.replace({
+          name: 'home'
+        })
+      }
+    },
+    // 发送验证码
     verifCode () {
       this.codeLoading = true
       verifCode({
         mobile: this.mobile
-      }).then(res => {
-        Toast.success('验证码发送成功，请注意查收')
-        this.codeLoading = false
-        this.codeStatus = true
-      }).catch((err) => {
-        if (!err) {
-          Toast.fail('获取失败，请重试')
-        }
-        this.codeLoading = false
       })
+        .then(res => {
+          Toast.success('验证码发送成功，请注意查收')
+          this.codeLoading = false
+          this.codeStatus = true
+        })
+        .catch(err => {
+          if (!err) {
+            Toast.fail('获取失败，请重试')
+          }
+          this.codeLoading = false
+        })
     },
-    /* 倒计时结束 */
+    // 倒计时结束
     countFinish () {
       this.countDownTime = 60000
       this.codeStatus = false
@@ -265,103 +264,109 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.page {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding-top: 84px;
-  flex: 1;
+.login-page {
   display: flex;
-  overflow: auto;
-  z-index: 1;
   flex-direction: column;
   align-items: center;
-  background-color: #333;
-  background-image: url("../../assets/imgs/login_bg.png");
   .van-nav-bar__arrow {
     position: fixed;
     left: 20px;
     top: 52px;
     color: #fff;
   }
-}
-.logo {
-  width: 212px;
-  height: 214px;
-  margin-bottom: 30px;
-}
-.logo-text {
-  width: 224px;
-  height: 86px;
-  margin-bottom: 80px;
-}
-.form {
-  width: 560px;
-  position: relative;
-  z-index: 9;
-  align-items: center;
-}
-.form-body {
-  width: 560px;
-  margin-top: 10px;
-}
-.form-bg {
-  position: absolute;
-  width: 560px;
-  left: 0;
-  height: 120px;
-  background-color: #fff;
-  opacity: 0.4;
-  border-radius: 10px;
-}
-.form-input {
-  flex-direction: row;
-  align-items: center;
-  width: 560px;
-  height: 120px;
-  border-radius: 10px;
-  padding-left: 30px;
-  padding-right: 20px;
-  margin-bottom: 30px;
-  border: none;
-  font-size: 34px;
-  background-color: rgba(255, 255, 255, 0.4);
-  .tf-icon {
+  .logo-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 339px;
+    background-image: url("../../assets/imgs/login_header.png");
+    background-size: contain;
+  }
+  .logo-container {
     display: flex;
-    line-height: 80px;
-    padding-left: 10px;
-    padding-right: 10px;
-    font-size: 36px;
+    justify-content: center;
+    align-items: center;
+    width: 190px;
+    height: 190px;
+    z-index: 1;
+    margin-top: 244px;
+    background: #ffffff;
+    box-shadow: 0px 15px 21px 0px rgba(199, 0, 23, 0.2);
+    border-radius: 50%;
+    .logo {
+      width: 140px;
+      height: 140px;
+    }
   }
-  /deep/ .van-field__control {
+}
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 560px;
+  margin-top: 86px;
+  .code-cell {
+    display: flex;
+    width: 100%;
+  }
+  .form-input {
+    align-items: center;
+    width: 100%;
+    height: 86px;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-bottom: 60px;
+    border: none;
+    border-bottom: 1px solid @red;
+    font-size: 42px;
+    background-color: rgba(255, 255, 255, 0.4);
+    .tf-icon {
+      display: flex;
+      line-height: 80px;
+      padding-left: 10px;
+      padding-right: 10px;
+      font-size: 36px;
+    }
+    /deep/ .van-field__control::placeholder {
+      font-size: 30px;
+      color: #383838 !important;
+    }
+  }
+  .query-code-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 160px;
+    height: 88px;
+    margin-left: 40px;
+    border: 2px solid @red;
+    border-radius: 4px;
+    font-size: 24px;
+    color: @red;
+  }
+  .code-count-down {
+    background: #f2f2f4;
+    color: #8f8f94;
+    border-color: #f2f2f4;
+  }
+  .login-btn {
+    width: 560px;
+    height: 88px;
+    margin-top: 36px;
+    margin-bottom: 40px;
+    background-image: linear-gradient(to right, @red, @red-dark);
+    border-radius: 44px;
+    border-width: 0;
+    font-size: 30px;
     color: #fff;
+    text-align: center;
   }
-}
-.phone-input {
-  flex: 1;
-  height: 120px;
-  font-size: 34px;
-  color: #fff;
-}
-.input-image {
-  width: 40px;
-  height: 40px;
-  margin-top: 22px;
-}
-.query-btn {
-  width: 100px;
-  height: 80px;
-  line-height: 80px;
-  border-width: 2px;
-  border-style: solid;
-  border-color: #fff;
-  border-radius: 10px;
-  text-align: center;
-  color: #fff;
-  background: none;
-  font-size: 24px;
+  .login-method__text {
+    font-size: 24px;
+    color: #8f8f94;
+    text-decoration: underline;
+  }
 }
 .agreement {
   margin-top: 160px;
@@ -370,43 +375,26 @@ export default {
   flex-direction: row;
   justify-content: center;
   align-items: center;
-}
-.uni-checkbox-input {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 5px;
-  border: 2px solid #fff;
-  border-radius: 3px;
-  width: 28px;
-  height: 28px;
-}
-.checkbox-icon {
-  font-size: 20px;
-}
-.tf-icon-gou {
-  color: #fff;
-}
-.login-method__text {
-  font-size: 24px;
-  font-weight: 400;
-  color: #fff;
-  text-decoration: underline;
-}
-.login-btn {
-  width: 560px;
-  height: 88px;
-  line-height: 88px;
-  background-image: linear-gradient(to right, @red, @red-dark);
-  border-radius: 10px;
-  border-width: 0;
-  font-size: 30px;
-  color: #fff;
-  text-align: center;
-  margin-top: 50px;
-  margin-bottom: 40px;
-}
-.agreement-text {
-  font-size: 24px;
+  .agreement-checkbox-input {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 28px;
+    height: 28px;
+    margin-right: 10px;
+    border: 2px solid #aaa;
+    .tf-icon-gou {
+      font-size: 24px;
+      color: #aaa;
+    }
+  }
+  .agreement-text {
+    font-size: 24px;
+    color: #8f8f94;
+    .agreement-link {
+      color: #8f8f94;
+      text-decoration: underline;
+    }
+  }
 }
 </style>
