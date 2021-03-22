dcbe9bb8ad2d13539a7aa4fdaa8531ef76c79605
hongbingkun <1810972990@qq.com>
2021-03-22 16:12:29 +0800

    feat：邻里详情新增视频播放

--- src/views/neighbours/details.vue
+++ src/views/neighbours/details.vue
@@ -1,147 +1,173 @@
 <template>
   <div class="tf-bg tf-body">
-    <van-nav-bar title="正文"
-                 :fixed="true"
-                 placeholder
-                 left-arrow
-                 @click-left="$router.go(-1)">
+    <van-nav-bar
+      title="正文"
+      :fixed="true"
+      placeholder
+      left-arrow
+      @click-left="$router.go(-1)"
+    >
       <template #right>
-        <span class="van-icon van-icon-ellipsis"
-              @click="openArticleDialog"></span>
+        <span
+          class="van-icon van-icon-ellipsis"
+          @click="openArticleDialog"
+        ></span>
       </template>
     </van-nav-bar>
-    <van-pull-refresh class="tf-body-container"
-                      success-text="刷新成功"
-                      v-model="isLoading"
-                      @refresh="onRefresh">
+    <van-pull-refresh
+      class="tf-body-container"
+      success-text="刷新成功"
+      v-model="isLoading"
+      @refresh="onRefresh"
+    >
       <div class="tf-card">
-        <div class="tf-card-header"
-             style="border-bottom:none"
-             :class="{ 'border-none': articleType == 2 || articleType == 3 }">
-          <userInfo :avatar="info.avatar"
-                    :name="info.nickname"
-                    :time="info.ctime">
-            <template v-if="articleType == 3"
-                      v-slot:right>
+        <div
+          class="tf-card-header"
+          style="border-bottom:none"
+          :class="{ 'border-none': articleType == 2 || articleType == 3 }"
+        >
+          <userInfo
+            :avatar="info.avatar"
+            :name="info.nickname"
+            :time="info.ctime"
+          >
+            <template v-if="articleType == 3" v-slot:right>
               <div class="group-tag">{{ info.category }}</div>
             </template>
           </userInfo>
         </div>
+        <!-- 资讯 -->
         <template v-if="articleType == 1">
           <div class="article-title">{{ info.title }}</div>
-          <div class="article-content"
-               v-html="info.content"></div>
-          <!-- 播放视屏  -->
-          <videoChild v-if="false"
-                      :url='videoURL'
-                      :poster="poster"></videoChild>
+          <div class="article-content-box">
+            <div class="article-content" v-html="info.content"></div>
+            <!-- 播放视屏  -->
+            <videoChild
+              v-if="info.video_url"
+              :url="info.video_url"
+              :poster="poster"
+              :style="{ order: info.video_site === '1' ? -1 : 2 }"
+            ></videoChild>
+          </div>
           <!-- <img class="activity-image" :src="info.thumbnail" /> -->
         </template>
+        <!-- 话题帖子 -->
         <template v-else-if="articleType == 3">
-          <tf-alert v-if="info.status == 2"
-                    type="warning"
-                    content="该内容含有违规信息。"
-                    :showRight="false"
-                    :showIcon="false"
-                    size="sm"></tf-alert>
+          <tf-alert
+            v-if="info.status == 2"
+            type="warning"
+            content="该内容含有违规信息。"
+            :showRight="false"
+            :showIcon="false"
+            size="sm"
+          ></tf-alert>
           <div class="tf-card-content">{{ info.content }}</div>
           <template v-if="info.images && info.images.length">
-            <img width="33%"
-                 :src="info.images[0]"
-                 v-if="info.images.length === 1" />
-            <tf-image-list v-else
-                           :data="info.images"
-                           mode="show"></tf-image-list>
+            <img
+              width="33%"
+              :src="info.images[0]"
+              v-if="info.images.length === 1"
+            />
+            <tf-image-list
+              v-else
+              :data="info.images"
+              mode="show"
+            ></tf-image-list>
           </template>
-          <!-- 播放视屏  -->
-          <videoChild v-if="false"
-                      :url='videoURL'
-                      :poster="poster"></videoChild>
         </template>
+        <!-- 活动 -->
         <template v-else-if="articleType == 2">
           <div class="article-title">{{ info.title }}</div>
           <div class="activity-content">
-            <div class="tf-text tf-mb-lg"
-                 v-html="info.content"></div>
-            <!-- <img class="activity-image" :src="info.thumbnail" /> -->
-            <!-- 播放视屏  -->
-            <videoChild v-if="false"
-                        :url='videoURL'
-                        :poster="poster"></videoChild>
-            <div class="apply-box"
-                 v-if="parseInt(info.joins) > 0">
+            <div class="activity-content-box tf-text tf-mb-lg">
+              <div v-html="info.content"></div>
+              <!-- <img class="activity-image" :src="info.thumbnail" /> -->
+              <!-- 播放视屏  -->
+              <videoChild
+                v-if="info.video_url"
+                :url="info.video_url"
+                :poster="poster"
+                :style="{ order: info.video_site === '1' ? -1 : 2 }"
+              ></videoChild>
+            </div>
+            <div class="apply-box" v-if="parseInt(info.joins) > 0">
               <div class="apply-title">
                 <strong>报名人员</strong>
                 <span class="tf-text-grey">({{ info.joins }}人)</span>
-                <span v-if="info.is_join"
-                      class="tf-status-tag">我已报名</span>
+                <span v-if="info.is_join" class="tf-status-tag">我已报名</span>
               </div>
               <div class="apply-user">
                 <template v-for="(item, i) in info.join_uids">
-                  <img v-if="item.avatar"
-                       class="apply-user__avatar"
-                       :src="item.avatar"
-                       :key="i" />
-                  <img v-else
-                       class="apply-user__avatar"
-                       src="@/assets/imgs/touxiang.png"
-                       :key="i" />
+                  <img
+                    v-if="item.avatar"
+                    class="apply-user__avatar"
+                    :src="item.avatar"
+                    :key="i"
+                  />
+                  <img
+                    v-else
+                    class="apply-user__avatar"
+                    src="@/assets/imgs/touxiang.png"
+                    :key="i"
+                  />
                 </template>
               </div>
             </div>
-            <div v-if="!info.is_join"
-                 v-preventReClick
-                 class="apply-btn"
-                 @click="joinActivity">
+            <div
+              v-if="!info.is_join"
+              v-preventReClick
+              class="apply-btn"
+              @click="joinActivity"
+            >
               报名
             </div>
           </div>
         </template>
-        <!-- 播放视屏 1111111 -->
-        <!-- <videoChild :url='videoURL'
-                    :poster="poster"></videoChild> -->
         <div class="activity-footer">
-          <div class="tf-icon tf-icon-zan"
-               :class="{ 'like-active': info.is_thumbsup }"
-               @click="thumbsUp(info)">
+          <div
+            class="tf-icon tf-icon-zan"
+            :class="{ 'like-active': info.is_thumbsup }"
+            @click="thumbsUp(info)"
+          >
             <span class="tf-text-sm">{{ info.thumbsups | numberText }}</span>
           </div>
-          <div class="tf-icon tf-icon-pinglun"
-               @click="$refs.reply.comment()">
+          <div class="tf-icon tf-icon-pinglun" @click="$refs.reply.comment()">
             <span class="tf-text-sm">{{ info.comments | numberText }}</span>
           </div>
         </div>
-
       </div>
-      <reply ref="reply"
-             class="tf-mt-lg activity-reply"
-             :is-mine="info.is_mine"
-             :articleId="id"
-             :articleType="articleType"
-             :thumbsupStatus="info.is_thumbsup"
-             @thumbsup="thumbsUp(info)"></reply>
+      <reply
+        ref="reply"
+        class="tf-mt-lg activity-reply"
+        :is-mine="info.is_mine"
+        :articleId="id"
+        :articleType="articleType"
+        :thumbsupStatus="info.is_thumbsup"
+        @thumbsup="thumbsUp(info)"
+      ></reply>
     </van-pull-refresh>
-    <more-popup v-model="moreShow"
-                :complain="!info.is_mine"
-                :complainType="1"
-                :complainInfo="info"
-                :shield="!info.is_mine"
-                :deleteProp="info.is_mine"
-                :share="true"
-                :share-obj="shareObj"
-                @delete="deleteArticle"></more-popup>
+    <more-popup
+      v-model="moreShow"
+      :complain="!info.is_mine"
+      :complainType="1"
+      :complainInfo="info"
+      :shield="!info.is_mine"
+      :deleteProp="info.is_mine"
+      :share="true"
+      :share-obj="shareObj"
+      @delete="deleteArticle"
+    ></more-popup>
   </div>
 </template>
 
 <script>
-import { NavBar, Popup, Toast, PullRefresh, Dialog } from 'vant'
 import UserInfo from '@/components/user-info/index.vue'
 import TfAlert from '@/components/tf-alert'
 import reply from './components/reply'
 import morePopup from './components/morePopup'
 import tfImageList from '@/components/tf-image-list'
-import { downloadPic } from '@/utils/util.js'
 import videoChild from '@/components/videoChild/videoChild'
+import { downloadPic } from '@/utils/util.js'
 import {
   getActivityInfo,
   joinActivity,
@@ -153,9 +179,6 @@ import {
 
 export default {
   components: {
-    [NavBar.name]: NavBar,
-    [Popup.name]: Popup,
-    [PullRefresh.name]: PullRefresh,
     UserInfo,
     TfAlert,
     reply,
@@ -165,23 +188,11 @@ export default {
   },
   data () {
     return {
-      videoURL: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4', // 视屏链接
-      poster: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png', // 视屏封面
-      articleType: 3,
+      poster: '', // 视屏封面
+      articleType: 0, // 文章类型：1资讯，2活动，3话题
       id: '',
-      moreShow: false,
-      info: {
-        id: '',
-        content: '',
-        images: [],
-        category: '',
-        nickname: '',
-        avatar: '',
-        thumbsups: '',
-        comments: '',
-        ctime: '',
-        share_img: ''
-      },
+      moreShow: false, // 更多操作弹窗
+      info: {},
       isLoading: false,
       shareObj: {}
     }
@@ -192,7 +203,6 @@ export default {
     this.id = id
     this.getInfo()
   },
-
   methods: {
     // 下拉刷新
     onRefresh () {
@@ -213,146 +223,104 @@ export default {
           break
       }
     },
-    /* 保存分享图片 */
+    // 获取资讯详情
+    getArticleInfo () {
+      getArticleInfo({
+        id: this.id
+      }).then(({ data }) => {
+        this.setInfo(data, '活动')
+      })
+    },
+    // 获取小组帖子详情
+    getPostBarInfo () {
+      getPostBarInfo({
+        id: this.id
+      })
+        .then(({ data }) => {
+          this.setInfo(data, '贴')
+          this.info.id = this.id
+        })
+        .catch(({ message }) => {
+          this.$toast.clear()
+          this.$dialog
+            .alert({
+              title: message || '该贴不存在'
+            })
+            .then(() => {
+              this.$router.go(-1)
+            })
+        })
+    },
+    // 获取活动详情
+    getActivityInfo () {
+      getActivityInfo({
+        id: this.id
+      }).then(({ data }) => {
+        this.setInfo(data, '活动')
+      })
+    },
+    // 获取完文章成功后
+    setInfo (data, text) {
+      if (this.goBackByNonentity(data.is_del, text)) {
+        return
+      }
+      this.info = data
+      this.isLoading = false
+      this.downloadSharePic()
+    },
+    // 文章不存在回退
+    goBackByNonentity (del, text) {
+      if (del === 1) {
+        this.$dialog
+          .alert({
+            title: `该${text}已被删除`
+          })
+          .then(() => {
+            this.$router.go(-1)
+          })
+        return true
+      }
+    },
+    // 保存分享图片
     downloadSharePic () {
       const that = this
       const urlName = 'detail_' + this.articleType + '_' + this.id
       downloadPic(this.info.share_img, urlName)
-        .then((data) => {
+        .then(data => {
           that.sendShareParam(data)
         })
         .catch(() => {
           that.sendShareParam('')
         })
     },
+    // 设置分享配置
     sendShareParam (data) {
       this.shareObj = {
         title: this.articleType == 3 ? this.info.content : this.info.title,
-        description: this.articleType == 3 ? this.info.content : this.info.title,
+        description:
+          this.articleType == 3 ? this.info.content : this.info.title,
         pyqTitle: this.articleType == 3 ? this.info.content : this.info.title,
         thumb: data ? 'fs://' + data + '.png' : '',
-        contentUrl: 'http://live.tosolomo.com/wap/#/neighbours?articleType=' + this.articleType + '&id=' + this.id
+        contentUrl:
+          'http://live.tosolomo.com/wap/#/neighbours?articleType=' +
+          this.articleType +
+          '&id=' +
+          this.id
       }
     },
-    /* 获取资讯详情 */
-    getArticleInfo () {
-      getArticleInfo({
-        id: this.id
-      }).then(({ data }) => {
-        if (data.is_del === 1) {
-          Dialog.alert({
-            title: '该活动已被删除'
-          }).then(() => {
-            this.$router.go(-1)
-          })
-          return
-        }
-        console.log('资讯详情', data)
-
-        this.info = data
-        // this.$nextTick(() => {
-        //   const aEle = document.getElementsByTagName('a')[0]
-        //   const img = document.querySelector('a img')
-        //   console.log(aEle)
-        //   console.log(img)
-        //   img.onclick = (e) => {
-        //     e.preventDefault()
-        //   }
-        //   aEle.onclick = () => {
-        //     return false
-        //   }
-        // })
-        this.isLoading = false
-        this.downloadSharePic()
-      })
-    },
-    /* 获取小组帖子详情 */
-    getPostBarInfo () {
-      getPostBarInfo({
-        id: this.id
-      })
-        .then(({ data }) => {
-          if (data.is_del === 1) {
-            Dialog.alert({
-              title: '该贴已被删除'
-            }).then(() => {
-              this.$router.go(-1)
-            })
-            return
-          }
-          console.log('帖子详情', data)
-
-          this.info = data
-          // this.$nextTick(() => {
-          //   const aEle = document.getElementsByTagName('a')[0]
-          //   const img = document.querySelector('a img')
-          //   console.log(aEle)
-          //   console.log(img)
-          //   img.onclick = (e) => {
-          //     e.preventDefault()
-          //   }
-          //   aEle.onclick = () => {
-          //     return false
-          //   }
-          // })
-          this.info.id = this.id
-          this.isLoading = false
-          this.downloadSharePic()
-        })
-        .catch(({ message }) => {
-          Toast.clear()
-          Dialog.alert({
-            title: message || '该贴不存在'
-          }).then(() => {
-            this.$router.go(-1)
-          })
-        })
-    },
-    /* 获取活动详情 */
-    getActivityInfo () {
-      getActivityInfo({
-        id: this.id
-      }).then(({ data }) => {
-        if (data.is_del === 1) {
-          Dialog.alert({
-            title: '该活动已被删除'
-          }).then(() => {
-            this.$router.go(-1)
-          })
-          return
-        }
-        console.log('活动详情', data)
-
-        this.info = data
-        // this.$nextTick(() => {
-        //   const aEle = document.getElementsByTagName('a')[0]
-        //   const img = document.querySelector('a img')
-        //   console.log(aEle)
-        //   console.log(img)
-        //   img.onclick = (e) => {
-        //     e.preventDefault()
-        //   }
-        //   aEle.onclick = () => {
-        //     return false
-        //   }
-        // })
-        this.isLoading = false
-        this.downloadSharePic()
-      })
-    },
-    /* 加入活动 */
+    // 加入活动
     joinActivity () {
       joinActivity({
         id: this.id
-      }).then((res) => {
-        Toast.success('活动报名成功')
+      }).then(res => {
+        this.$toast.success('活动报名成功')
         this.getInfo()
         this.mtjEvent({
           eventId: 8
         })
       })
     },
-    /* 点赞 */
+    // 点赞
     thumbsUp (item) {
       // 判断是否点过赞，点过赞无法取消
       if (item.is_thumbsup) {
@@ -361,7 +329,7 @@ export default {
       thumbsUp({
         id: this.id,
         t_type: 1
-      }).then((res) => {
+      }).then(res => {
         // 点赞图标点亮
         item.thumbsups++
         item.is_thumbsup = 1
@@ -374,23 +342,24 @@ export default {
     openArticleDialog () {
       this.moreShow = true
     },
-    /* 删除帖子 */
+    // 删除帖子
     deleteArticle () {
       deleteArticle({
         id: this.id
-      }).then((res) => {
-        Dialog.alert({
-          title: '删除成功'
-        }).then(() => {
-          this.$router.go(-1)
-          this.mtjEvent({
-            eventId: 44
+      }).then(res => {
+        this.$dialog
+          .alert({
+            title: '删除成功'
           })
-        })
+          .then(() => {
+            this.$router.go(-1)
+            this.mtjEvent({
+              eventId: 44
+            })
+          })
         this.moreShow = false
       })
     }
-
   },
   filters: {
     numberText (value) {
@@ -405,7 +374,7 @@ export default {
   },
   watch: {
     // 监听 this.$route.query
-    '$route.query': function (newVal, oldVal) {
+    '$route.query' (newVal) {
       this.articleType = newVal.articleType
       this.id = newVal.id
       this.getInfo()
@@ -471,15 +440,19 @@ export default {
   border-bottom: none;
 }
 .article-title {
-  font-size: 34px;
-  font-weight: bold;
   margin-top: 10px;
   margin-bottom: 30px;
+  font-size: 34px;
+  font-weight: bold;
   word-break: break-all;
 }
+.article-content-box {
+  display: flex;
+  flex-direction: column;
+  padding-top: 40px;
+  border-top: 2px solid @divider-color;
+}
 .article-content {
-  padding: 40px 0 10px;
-  border-top: 1px solid @divider-color;
   font-size: 28px;
   color: #666;
   word-break: break-all;
@@ -493,6 +466,10 @@ export default {
   border-top: 1px solid @divider-color;
   padding-top: 30px;
   word-break: break-all;
+  .activity-content-box {
+    @flex-column();
+    width: 100%;
+  }
   /deep/ img {
     max-width: 100% !important;
   }
